8921f36ed2d72e77fed8ccd4b7545b1fdefd065a
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2018-10-24 16:42:40 +0200

    feat(keyboard-navigation): allow to move canvas with keyboard at different speed levels
    * Default listener will use `config.keyboardNavigation.moveSpeedModified`
      to determine the speed of movement with keyboard when Shift is pressed.
    * Default value for modified movement speed is 200.

--- test/spec/features/keyboard-navigation/KeyboardNavigationSpec.js
+++ test/spec/features/keyboard-navigation/KeyboardNavigationSpec.js
@@ -47,7 +47,6 @@ describe('features/keyboard-navigation', function() {
 
   });
 
-
   describe('arrow bindings', function() {
 
     var KEYS = {
@@ -84,64 +83,128 @@ describe('features/keyboard-navigation', function() {
 
       describe('with Ctrl/Cmd', function() {
 
-        var decisionTable = [
-          {
-            desc: 'move left',
-            keys: KEYS.LEFT,
-            x: -50,
-            y: 0
-          },
-          {
-            desc: 'move right',
-            keys: KEYS.RIGHT,
-            x: 50,
-            y: 0
-          },
-          {
-            desc: 'move up',
-            keys: KEYS.UP,
-            x: 0,
-            y: -50
-          },
-          {
-            desc: 'move down',
-            keys: KEYS.DOWN,
-            x: 0,
-            y: 50
-          }
-        ];
-        /* eslint-enable */
+        var BASE_SPEED = 50,
+            HIGH_SPEED = 200;
 
-        forEach(decisionTable, function(testCase) {
+        describe('without shift', function() {
 
-          forEach(testCase.keys, function(key) {
+          var decisionTable = [
+            {
+              desc: 'move left',
+              keys: KEYS.LEFT,
+              x: -BASE_SPEED,
+              y: 0
+            },
+            {
+              desc: 'move right',
+              keys: KEYS.RIGHT,
+              x: BASE_SPEED,
+              y: 0
+            },
+            {
+              desc: 'move up',
+              keys: KEYS.UP,
+              x: 0,
+              y: -BASE_SPEED
+            },
+            {
+              desc: 'move down',
+              keys: KEYS.DOWN,
+              x: 0,
+              y: BASE_SPEED
+            }
+          ];
 
-            it('should ' + testCase.desc + ' with CmdKey', inject(function(canvas, keyboard) {
+          forEach(decisionTable, function(testCase) {
 
-              // given
-              var event = createKeyEvent(key, { metaKey: true, shiftKey: false });
+            forEach(testCase.keys, function(key) {
 
-              // when
-              keyboard._keyHandler(event);
+              it('should ' + testCase.desc + ' with CtrlKey', inject(function(canvas, keyboard) {
 
-              // then
-              expect(canvas.viewbox().x).to.eql(testCase.x);
-              expect(canvas.viewbox().y).to.eql(testCase.y);
-            }));
+                // given
+                var event = createKeyEvent(key, {
+                  ctrlKey: true,
+                  shiftKey: false
+                });
+
+                // when
+                keyboard._keyHandler(event);
+
+                // then
+                expect(canvas.viewbox().x).to.eql(testCase.x);
+                expect(canvas.viewbox().y).to.eql(testCase.y);
+              }));
 
 
-            it('should ' + testCase.desc + ' with CtrlKey', inject(function(canvas, keyboard) {
+              it('should ' + testCase.desc + ' with CmdKey', inject(function(canvas, keyboard) {
 
-              // given
-              var event = createKeyEvent(key, { ctrlKey: true, shiftKey: false });
+                // given
+                var event = createKeyEvent(key, {
+                  metaKey: true,
+                  shiftKey: false
+                });
 
-              // when
-              keyboard._keyHandler(event);
+                // when
+                keyboard._keyHandler(event);
 
-              // then
-              expect(canvas.viewbox().x).to.eql(testCase.x);
-              expect(canvas.viewbox().y).to.eql(testCase.y);
-            }));
+                // then
+                expect(canvas.viewbox().x).to.eql(testCase.x);
+                expect(canvas.viewbox().y).to.eql(testCase.y);
+              }));
+
+            });
+
+          });
+
+        });
+
+        describe('with shift', function() {
+
+          var decisionTable = [
+            {
+              desc: 'move left',
+              keys: KEYS.LEFT,
+              x: -HIGH_SPEED,
+              y: 0
+            },
+            {
+              desc: 'move right',
+              keys: KEYS.RIGHT,
+              x: HIGH_SPEED,
+              y: 0
+            },
+            {
+              desc: 'move up',
+              keys: KEYS.UP,
+              x: 0,
+              y: -HIGH_SPEED
+            },
+            {
+              desc: 'move down',
+              keys: KEYS.DOWN,
+              x: 0,
+              y: HIGH_SPEED
+            },
+          ];
+
+          forEach(decisionTable, function(testCase) {
+
+            forEach(testCase.keys, function(key) {
+
+              it('should ' + testCase.desc, inject(function(canvas, keyboard) {
+
+                // given
+                var event = createKeyEvent(key, { ctrlKey: true, shiftKey: true });
+
+                // when
+                keyboard._keyHandler(event);
+
+                // then
+                expect(canvas.viewbox().x).to.eql(testCase.x);
+                expect(canvas.viewbox().y).to.eql(testCase.y);
+              }));
+
+            });
 
           });
 
@@ -175,6 +238,32 @@ describe('features/keyboard-navigation', function() {
           // then
           expect(canvas.viewbox().x).to.eql(0);
           expect(canvas.viewbox().y).to.eql(23);
+
+        });
+      });
+
+
+      it('should use custom modified speed', function() {
+
+        // given
+        var customConfig = {
+          keyboardNavigation: {
+            moveSpeedModified: 1
+          }
+        };
+
+        bootstrapDiagram(assign({}, defaultDiagramConfig, customConfig))();
+
+        var keyDownEvent = createKeyEvent(KEYS.DOWN[0], { ctrlKey: true, shiftKey: true });
+
+        getDiagramJS().invoke(function(canvas, keyboard) {
+
+          // when
+          keyboard._keyHandler(keyDownEvent);
+
+          // then
+          expect(canvas.viewbox().x).to.eql(0);
+          expect(canvas.viewbox().y).to.eql(1);
 
         });
       });
